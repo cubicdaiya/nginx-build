9cc2b6cfda4911e34707861ad7579676543d8b91
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2014-07-12 23:39:07 +0900

    fixed failure to test

--- module3rd_test.go
+++ module3rd_test.go
@@ -29,7 +29,7 @@ func (suite *Module3rdTestSuite) TestModules3rd() {
 		case "echo-nginx-module":
 			assert.Equal(suite.T(), m.Name, "echo-nginx-module")
 			assert.Equal(suite.T(), m.Url, "https://github.com/openresty/echo-nginx-module.git")
-			assert.Equal(suite.T(), m.Rev, "v0.53")
+			assert.Equal(suite.T(), m.Rev, "v0.54")
 		case "headers-more-nginx-module":
 			assert.Equal(suite.T(), m.Name, "headers-more-nginx-module")
 			assert.Equal(suite.T(), m.Url, "https://github.com/openresty/headers-more-nginx-module.git")
@@ -53,7 +53,7 @@ func (suite *Module3rdTestSuite) TestModules3rd() {
 		case "ngx_small_light":
 			assert.Equal(suite.T(), m.Name, "ngx_small_light")
 			assert.Equal(suite.T(), m.Url, "https://github.com/cubicdaiya/ngx_small_light")
-			assert.Equal(suite.T(), m.Rev, "v0.5.2")
+			assert.Equal(suite.T(), m.Rev, "v0.5.3")
 			assert.Equal(suite.T(), m.Shprov, "./setup --with-gd")
 		}
 	}
