a24f0da09a2e7d32f161c62d966227862f7b2807
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2015-02-11 00:46:47 +0900

    bumped nginx version to 1.7.10.

--- const.go
+++ const.go
@@ -4,7 +4,7 @@ const NGINX_BUILD_VERSION = "0.3.1"
 
 // nginx
 const (
-	NGINX_VERSION             = "1.7.9"
+	NGINX_VERSION             = "1.7.10"
 	NGINX_DOWNLOAD_URL_PREFIX = "http://nginx.org/download"
 )
 
