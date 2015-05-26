b06e588de88a26c0f9db63578afe6067aa0e6e41
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2015-05-26 23:28:44 +0900

    bumped nginx version to 1.9.1.

--- const.go
+++ const.go
@@ -4,7 +4,7 @@ const NGINX_BUILD_VERSION = "0.3.4"
 
 // nginx
 const (
-	NGINX_VERSION             = "1.9.0"
+	NGINX_VERSION             = "1.9.1"
 	NGINX_DOWNLOAD_URL_PREFIX = "http://nginx.org/download"
 )
 
