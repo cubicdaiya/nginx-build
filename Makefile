7ae6146a3292863ab90cbf89b07b0fa9a45113dc
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2015-06-17 08:27:55 +0900

    bumped nginx-build version to 0.4.1.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=0.4.0
+VERSION=0.4.1
 
 nginx-build: *.go
 	gom build -o nginx-build
--- const.go
+++ const.go
@@ -1,6 +1,6 @@
 package main
 
-const NGINX_BUILD_VERSION = "0.4.0"
+const NGINX_BUILD_VERSION = "0.4.1"
 
 // nginx
 const (
