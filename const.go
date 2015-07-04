1eeb11175fae1e3e80b0cb58fccfc861bb2ad413
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2015-07-04 10:26:59 +0900

    bumped openresty version to 1.7.10.2.

--- const.go
+++ const.go
@@ -28,7 +28,7 @@ const (
 
 // openResty
 const (
-	OPENRESTY_VERSION             = "1.7.10.1"
+	OPENRESTY_VERSION             = "1.7.10.2"
 	OPENRESTY_DOWNLOAD_URL_PREFIX = "http://openresty.org/download"
 )
 
