85841f6610ff16394615647fda97f6d1a87fd240
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2015-10-01 01:17:22 +0900

    bumped version to 0.6.0.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=0.5.0
+VERSION=0.6.0
 
 nginx-build: *.go
 	gom build -o nginx-build
--- const.go
+++ const.go
@@ -1,6 +1,6 @@
 package main
 
-const NGINX_BUILD_VERSION = "0.5.0"
+const NGINX_BUILD_VERSION = "0.6.0"
 
 // nginx
 const (
