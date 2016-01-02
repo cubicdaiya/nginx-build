42113a1d124af3a391ed559dbdf57b23efaa7268
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2016-01-03 08:55:10 +0900

    updated copyright year.

--- doc/conf.py
+++ doc/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'nginx-build'
-copyright = u'2014,2015 Tatsuhiko Kubo'
+copyright = u'2014-2016 Tatsuhiko Kubo'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
