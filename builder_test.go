fa698d4fe222add8febbac34cbf18e61da425244
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2016-02-01 11:52:27 +0900

    fixed commit leak of test code for c105713.

--- builder_test.go
+++ builder_test.go
@@ -29,7 +29,7 @@ func (suite *BuilderTestSuite) TestName() {
 	assert.Equal(suite.T(), suite.builders[COMPONENT_PCRE].name(), "pcre")
 	assert.Equal(suite.T(), suite.builders[COMPONENT_OPENSSL].name(), "openssl")
 	assert.Equal(suite.T(), suite.builders[COMPONENT_ZLIB].name(), "zlib")
-	assert.Equal(suite.T(), suite.builders[COMPONENT_OPENRESTY].name(), "ngx_openresty")
+	assert.Equal(suite.T(), suite.builders[COMPONENT_OPENRESTY].name(), "openresty")
 	assert.Equal(suite.T(), suite.builders[COMPONENT_TENGINE].name(), "tengine")
 }
 
@@ -44,7 +44,7 @@ func (suite *BuilderTestSuite) TestDownloadURL() {
 	assert.Equal(suite.T(), suite.builders[COMPONENT_PCRE].downloadURL(), fmt.Sprintf("%s/pcre-%s.tar.gz", PCRE_DOWNLOAD_URL_PREFIX, PCRE_VERSION))
 	assert.Equal(suite.T(), suite.builders[COMPONENT_OPENSSL].downloadURL(), fmt.Sprintf("%s/openssl-%s.tar.gz", OPENSSL_DOWNLOAD_URL_PREFIX, OPENSSL_VERSION))
 	assert.Equal(suite.T(), suite.builders[COMPONENT_ZLIB].downloadURL(), fmt.Sprintf("%s/zlib-%s.tar.gz", ZLIB_DOWNLOAD_URL_PREFIX, ZLIB_VERSION))
-	assert.Equal(suite.T(), suite.builders[COMPONENT_OPENRESTY].downloadURL(), fmt.Sprintf("%s/ngx_openresty-%s.tar.gz", OPENRESTY_DOWNLOAD_URL_PREFIX, OPENRESTY_VERSION))
+	assert.Equal(suite.T(), suite.builders[COMPONENT_OPENRESTY].downloadURL(), fmt.Sprintf("%s/openresty-%s.tar.gz", OPENRESTY_DOWNLOAD_URL_PREFIX, OPENRESTY_VERSION))
 	assert.Equal(suite.T(), suite.builders[COMPONENT_TENGINE].downloadURL(), fmt.Sprintf("%s/tengine-%s.tar.gz", TENGINE_DOWNLOAD_URL_PREFIX, TENGINE_VERSION))
 }
 
@@ -53,7 +53,7 @@ func (suite *BuilderTestSuite) TestSourcePath() {
 	assert.Equal(suite.T(), suite.builders[COMPONENT_PCRE].sourcePath(), fmt.Sprintf("pcre-%s", PCRE_VERSION))
 	assert.Equal(suite.T(), suite.builders[COMPONENT_OPENSSL].sourcePath(), fmt.Sprintf("openssl-%s", OPENSSL_VERSION))
 	assert.Equal(suite.T(), suite.builders[COMPONENT_ZLIB].sourcePath(), fmt.Sprintf("zlib-%s", ZLIB_VERSION))
-	assert.Equal(suite.T(), suite.builders[COMPONENT_OPENRESTY].sourcePath(), fmt.Sprintf("ngx_openresty-%s", OPENRESTY_VERSION))
+	assert.Equal(suite.T(), suite.builders[COMPONENT_OPENRESTY].sourcePath(), fmt.Sprintf("openresty-%s", OPENRESTY_VERSION))
 	assert.Equal(suite.T(), suite.builders[COMPONENT_TENGINE].sourcePath(), fmt.Sprintf("tengine-%s", TENGINE_VERSION))
 }
 
@@ -62,7 +62,7 @@ func (suite *BuilderTestSuite) TestArchivePath() {
 	assert.Equal(suite.T(), suite.builders[COMPONENT_PCRE].archivePath(), fmt.Sprintf("pcre-%s.tar.gz", PCRE_VERSION))
 	assert.Equal(suite.T(), suite.builders[COMPONENT_OPENSSL].archivePath(), fmt.Sprintf("openssl-%s.tar.gz", OPENSSL_VERSION))
 	assert.Equal(suite.T(), suite.builders[COMPONENT_ZLIB].archivePath(), fmt.Sprintf("zlib-%s.tar.gz", ZLIB_VERSION))
-	assert.Equal(suite.T(), suite.builders[COMPONENT_OPENRESTY].archivePath(), fmt.Sprintf("ngx_openresty-%s.tar.gz", OPENRESTY_VERSION))
+	assert.Equal(suite.T(), suite.builders[COMPONENT_OPENRESTY].archivePath(), fmt.Sprintf("openresty-%s.tar.gz", OPENRESTY_VERSION))
 	assert.Equal(suite.T(), suite.builders[COMPONENT_TENGINE].archivePath(), fmt.Sprintf("tengine-%s.tar.gz", TENGINE_VERSION))
 }
 
@@ -71,7 +71,7 @@ func (suite *BuilderTestSuite) TestLogPath() {
 	assert.Equal(suite.T(), suite.builders[COMPONENT_PCRE].logPath(), fmt.Sprintf("pcre-%s.log", PCRE_VERSION))
 	assert.Equal(suite.T(), suite.builders[COMPONENT_OPENSSL].logPath(), fmt.Sprintf("openssl-%s.log", OPENSSL_VERSION))
 	assert.Equal(suite.T(), suite.builders[COMPONENT_ZLIB].logPath(), fmt.Sprintf("zlib-%s.log", ZLIB_VERSION))
-	assert.Equal(suite.T(), suite.builders[COMPONENT_OPENRESTY].logPath(), fmt.Sprintf("ngx_openresty-%s.log", OPENRESTY_VERSION))
+	assert.Equal(suite.T(), suite.builders[COMPONENT_OPENRESTY].logPath(), fmt.Sprintf("openresty-%s.log", OPENRESTY_VERSION))
 	assert.Equal(suite.T(), suite.builders[COMPONENT_TENGINE].logPath(), fmt.Sprintf("tengine-%s.log", TENGINE_VERSION))
 }
 
