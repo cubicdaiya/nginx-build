d607974e6ceb34c93df870a49b5e85e8ea54014e
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2016-02-25 00:42:44 +0900

    bumped version to 0.7.1.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=0.7.0
+VERSION=0.7.1
 
 nginx-build: *.go
 	gom build -o nginx-build
--- const.go
+++ const.go
@@ -1,6 +1,6 @@
 package main
 
-const NGINX_BUILD_VERSION = "0.7.0"
+const NGINX_BUILD_VERSION = "0.7.1"
 
 // nginx
 const (
