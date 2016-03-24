cubicdaiya/nginx-build

commit 2f342cf81e3924784e0f9efce144dd7e923836d9

allowed multiple -add-dynamic-module=PATH.

nginx-build.go
@@ -45,6 +45,8 @@ func main() {
 	for k, v := range argsString {
 		if k == "add-module" {
 			flag.Var(&multiflag, k, v.Desc)
+		} else if k == "add-dynamic-module" {
+			flag.Var(&multiflag, k, v.Desc)
 		} else {
 			v.Value = flag.String(k, "", v.Desc)
 			argsString[k] = v
@@ -65,6 +67,14 @@ func main() {
 		tmp_ := multiflag.String()
 		tmp.Value = &tmp_
 		argsString["add-module"] = tmp
+	}
+
+	// Allow multiple flags for `--add-dynamic-module`
+	{
+		tmp := argsString["add-dynamic-module"]
+		tmp_ := multiflag.String()
+		tmp.Value = &tmp_
+		argsString["add-dynamic-module"] = tmp
 	}
 
 	configureOptions.Values = argsString

commit 0e7daed5a5455bc393e6bb28a276a4e2e0ec4793

fixed broken configure in 2f342cf. refs #6.
The option name is typoed and the values of both
--add-module and --add-dynamic-module are shared by mistake.

configureopt.go
@@ -424,7 +424,7 @@ func makeArgsString() map[string]ConfigureOptionValue {
 		Desc: "enable external module",
 	}
 	argsString["add-dynamic-module"] = ConfigureOptionValue{
-		Name: "--add-dynamic module",
+		Name: "--add-dynamic-module",
 		Desc: "enable dynamic external module",
 	}
 	argsString["with-cc"] = ConfigureOptionValue{

nginx-build.go
@@ -36,16 +36,19 @@ func main() {
 	tengineVersion := flag.String("tengineversion", TENGINE_VERSION, "tengine version")
 	configureOnly := flag.Bool("configureonly", false, "configuring nginx only not building")
 
-	var configureOptions ConfigureOptions
 	argsString := makeArgsString()
 	argsBool := makeArgsBool()
-	var multiflag StringFlag
+	var (
+		configureOptions ConfigureOptions
+		multiflag StringFlag
+		multiflagDynamic StringFlag
+	)
 
 	for k, v := range argsString {
 		if k == "add-module" {
 			flag.Var(&multiflag, k, v.Desc)
 		} else if k == "add-dynamic-module" {
-			flag.Var(&multiflag, k, v.Desc)
+			flag.Var(&multiflagDynamic, k, v.Desc)
 		} else {
 			v.Value = flag.String(k, "", v.Desc)
 			argsString[k] = v
@@ -71,7 +74,7 @@ func main() {
 	// Allow multiple flags for `--add-dynamic-module`
 	{
 		tmp := argsString["add-dynamic-module"]
-		tmp_ := multiflag.String()
+		tmp_ := multiflagDynamic.String()
 		tmp.Value = &tmp_
 		argsString["add-dynamic-module"] = tmp
 	}