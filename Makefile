2b75fd3c7d91350b27a17a4ac64bf0a41f8c0e25
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2016-04-13 11:07:35 +0900

    bumped version to 0.8.0.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=0.7.3
+VERSION=0.8.0
 
 nginx-build: *.go
 	gom build -o nginx-build
--- const.go
+++ const.go
@@ -1,6 +1,6 @@
 package main
 
-const NGINX_BUILD_VERSION = "0.7.3"
+const NGINX_BUILD_VERSION = "0.8.0"
 
 // nginx
 const (
