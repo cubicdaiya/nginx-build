92da643bd792d33e82ffca53045ce006cfa674ee
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2016-04-30 13:38:31 +0900

    cutomized flag.Usage().
    The output of original flag.Usage() is too long.

---
+++ option.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"runtime"
+	"strconv"
+
+	"github.com/cubicdaiya/nginx-build/builder"
+)
+
+type Options struct {
+	Values  map[string]OptionValue
+	Bools   map[string]OptionBool
+	Numbers map[string]OptionNumber
+}
+
+type OptionValue struct {
+	Desc    string
+	Value   *string
+	Default string
+}
+
+type OptionBool struct {
+	Desc    string
+	Enabled *bool
+	// all options are false by default
+	//Default bool
+}
+
+type OptionNumber struct {
+	Desc    string
+	Value   *int
+	Default int
+}
+
+func makeNginxBuildOptions() Options {
+	var nginxBuildOptions Options
+	argsNumber := make(map[string]OptionNumber)
+	argsBool := make(map[string]OptionBool)
+	argsString := make(map[string]OptionValue)
+
+	argsNumber["j"] = OptionNumber{
+		Desc:    "jobs to build nginx",
+		Default: runtime.NumCPU(),
+	}
+
+	argsBool["verbose"] = OptionBool{
+		Desc: "verbose mode",
+	}
+	argsBool["pcre"] = OptionBool{
+		Desc: "embedded PCRE staticlibrary",
+	}
+	argsBool["openssl"] = OptionBool{
+		Desc: "embedded OpenSSL staticlibrary",
+	}
+	argsBool["zlib"] = OptionBool{
+		Desc: "embedded zlib staticlibrary",
+	}
+	argsBool["clear"] = OptionBool{
+		Desc: "remove entries in working directory",
+	}
+	argsBool["version"] = OptionBool{
+		Desc: "print nginx-build version",
+	}
+	argsBool["versions"] = OptionBool{
+		Desc: "print nginx versions",
+	}
+	argsBool["openresty"] = OptionBool{
+		Desc: "download openresty instead of nginx",
+	}
+	argsBool["tengine"] = OptionBool{
+		Desc: "download tengine instead of nginx",
+	}
+	argsBool["configureonly"] = OptionBool{
+		Desc: "configure nginx only not building",
+	}
+
+	argsString["v"] = OptionValue{
+		Desc:    "nginx version",
+		Default: builder.NGINX_VERSION,
+	}
+	argsString["c"] = OptionValue{
+		Desc:    "configuration file for building nginx",
+		Default: "",
+	}
+	argsString["m"] = OptionValue{
+		Desc:    "configuration file for 3rd party modules",
+		Default: "",
+	}
+	argsString["d"] = OptionValue{
+		Desc:    "working directory",
+		Default: "",
+	}
+	argsString["pcreversion"] = OptionValue{
+		Desc:    "PCRE version",
+		Default: builder.PCRE_VERSION,
+	}
+	argsString["opensslversion"] = OptionValue{
+		Desc:    "OpenSSL version",
+		Default: builder.OPENSSL_VERSION,
+	}
+	argsString["zlibversion"] = OptionValue{
+		Desc:    "zlib version",
+		Default: builder.ZLIB_VERSION,
+	}
+	argsString["openrestyversion"] = OptionValue{
+		Desc:    "openresty version",
+		Default: builder.OPENRESTY_VERSION,
+	}
+	argsString["tengineversion"] = OptionValue{
+		Desc:    "tengine version",
+		Default: builder.TENGINE_VERSION,
+	}
+
+	nginxBuildOptions.Bools = argsBool
+	nginxBuildOptions.Values = argsString
+	nginxBuildOptions.Numbers = argsNumber
+
+	return nginxBuildOptions
+}
+
+func isNginxBuildOption(k string) bool {
+	if _, ok := nginxBuildOptions.Bools[k]; ok {
+		return true
+	}
+	if _, ok := nginxBuildOptions.Values[k]; ok {
+		return true
+	}
+	if _, ok := nginxBuildOptions.Numbers[k]; ok {
+		return true
+	}
+	return false
+}
+
+func defaultStringValue(k string) string {
+	if _, ok := nginxBuildOptions.Values[k]; ok {
+		return nginxBuildOptions.Values[k].Default
+	}
+	if _, ok := nginxBuildOptions.Numbers[k]; ok {
+		return strconv.Itoa(nginxBuildOptions.Numbers[k].Default)
+	}
+	return ""
+}
