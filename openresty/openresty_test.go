b9b7ba4ca3f525307d6826f4439d1364847d51f8
Guilherme Salazar <gsz@acm.org>
2019-05-09 11:00:55 -0700

    openresty: add release candidate test case

--- openresty/openresty_test.go
+++ openresty/openresty_test.go
@@ -12,4 +12,5 @@ func TestOpenrestyName(t *testing.T) {
 	assert.Equal("ngx_openresty", Name("1.9.7.2"))
 	assert.Equal("openresty", Name("1.9.7.3"))
 	assert.Equal("openresty", Name("1.9.7.4"))
+	assert.Equal("openresty", Name("1.15.8.1rc1"))
 }
