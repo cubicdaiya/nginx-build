f266824c0dcd0aa9f1da8d600f66ec73529ef619
catatsuy <catatsuy@catatsuy.org>
2020-03-21 18:07:29 +0900

    bumped default LibreSSL version to 3.0.2
    refs:
    https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-3.0.2-relnotes.txt

--- builder/const.go
+++ builder/const.go
@@ -20,7 +20,7 @@ const (
 
 // libressl
 const (
-	LibreSSLVersion           = "2.9.2"
+	LibreSSLVersion           = "3.0.2"
 	LibreSSLDownloadURLPrefix = "https://ftp.openbsd.org/pub/OpenBSD/LibreSSL"
 )
 
