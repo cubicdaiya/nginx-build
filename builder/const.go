3a4484294b3951da4df92f31fbfb13429c3a4dee
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2021-06-08 10:00:51 +0900

    bumped default openresty version to 1.19.3.2.

--- builder/const.go
+++ builder/const.go
@@ -32,7 +32,7 @@ const (
 
 // openResty
 const (
-	OpenRestyVersion           = "1.19.3.1"
+	OpenRestyVersion           = "1.19.3.2"
 	OpenRestyDownloadURLPrefix = "https://openresty.org/download"
 )
 
