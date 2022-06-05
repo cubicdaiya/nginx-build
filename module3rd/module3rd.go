35707db82d03517ebb5bf9594ee340c135ba8f18
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2022-06-05 17:59:44 +0900

    module3rd: anotated tags for json format.

--- module3rd/module3rd.go
+++ module3rd/module3rd.go
@@ -1,11 +1,11 @@
 package module3rd
 
 type Module3rd struct {
-	Name      string
-	Form      string
-	Url       string
-	Rev       string
-	Dynamic   bool
-	Shprov    string
-	ShprovDir string
+	Name      string `json:"name"`
+	Form      string `json:"form"`
+	Url       string `json:"url"`
+	Rev       string `json:"rev"`
+	Dynamic   bool   `json:"dynamic"`
+	Shprov    string `json:"shprov"`
+	ShprovDir string `json:"shprovdir"`
 }
