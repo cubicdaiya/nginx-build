9eacd7db3dedeb9c1f21fc62311f9e13aedb890a
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2022-06-05 17:30:24 +0900

    module3rd: changed configuration format from ini to json.
    nginx-build originally had a lot of dependent modules.
    To migitate the maintenance load, this change reduces them.
    Because json handling is available by only official package(encoding/json).

--- config/modules.cfg.njs
+++
@@ -1,5 +0,0 @@
-[njs/nginx]
-form=hg
-url=https://hg.nginx.org/njs
-shprov=./configure && make
-shprovdir=..
--- module3rd/load_test.go
+++ module3rd/load_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestModules3rd(t *testing.T) {
 
-	modules3rdConf := "../config/modules.cfg.example"
+	modules3rdConf := "../config/modules.json.example"
 	modules3rd, err := Load(modules3rdConf)
 	if err != nil {
 		t.Fatalf("Failed to load %s", modules3rdConf)
@@ -20,6 +20,34 @@ func TestModules3rd(t *testing.T) {
 			want.Form = "git"
 			want.Url = "https://github.com/cubicdaiya/ngx_http_hello_world"
 			want.Dynamic = false
+		default:
+			t.Fatalf("unexpected module: %v", m)
+		}
+
+		if m != want {
+			t.Fatalf("got: %v, want: %v", m, want)
+		}
+	}
+}
+
+func TestModules3rdWithNJS(t *testing.T) {
+
+	modules3rdConf := "../config/modules.json.njs"
+	modules3rd, err := Load(modules3rdConf)
+	if err != nil {
+		t.Fatalf("Failed to load %s", modules3rdConf)
+	}
+
+	for _, m := range modules3rd {
+		var want Module3rd
+		switch m.Name {
+		case "njs/nginx":
+			want.Name = "njs/nginx"
+			want.Form = "hg"
+			want.Url = "https://hg.nginx.org/njs"
+			want.Dynamic = false
+			want.Shprov = "./configure && make"
+			want.ShprovDir = ".."
 		default:
 			t.Fatalf("unexpected module: %v", m)
 		}
