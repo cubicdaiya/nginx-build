f7c4e22b8649c5bce44a2006468d5f2e25350475
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2022-06-10 14:38:38 +0900

    module3rd: refactored.

--- module3rd/load.go
+++ module3rd/load.go
@@ -4,21 +4,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-
-	"github.com/cubicdaiya/nginx-build/util"
 )
 
 func Load(path string) ([]Module3rd, error) {
 	var modules []Module3rd
 	if len(path) > 0 {
-		if !util.FileExists(path) {
-			return modules, fmt.Errorf("modulesConfPath(%s) does not exist.", path)
-		}
-		data, err := os.ReadFile(path)
+		f, err := os.Open(path)
 		if err != nil {
 			return modules, err
 		}
-		if err := json.Unmarshal(data, &modules); err != nil {
+		if err := json.NewDecoder(f).Decode(&modules); err != nil {
 			return modules, fmt.Errorf("modulesConfPath(%s) is invalid JSON.", path)
 		}
 		for i, _ := range modules {
